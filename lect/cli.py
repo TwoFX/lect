19e8f83a4626d34780cbc01f1166b3bdfd824efc
Markus Himmel <markus@himmel-villmar.de>
2020-06-11 15:56:28 +0200

    Very basic functionality

--- lect/cli.py
+++ lect/cli.py
@@ -7,21 +7,29 @@ def main():
 
 def get_node_info(current_directory):
     with (current_directory / 'info.yaml').open() as f:
-        return yaml.load(f)
+        return yaml.safe_load(f)
 
 latex_env = jinja2.Environment(
     loader = jinja2.PackageLoader(__name__, 'templates-latex'),
     autoescape = False,
+    trim_blocks = True,
     variable_start_string = '\VAR{',
-    variable_end_string = '}')
+    variable_end_string = '}',
+    line_statement_prefix='%%',
+    block_start_string = '\BLOCK{',
+    block_end_string = '}')
 
 def handle(current_directory):
     info = get_node_info(current_directory)
+
+    if 'children' in info:
+        info['rendered_children'] = map(lambda subdir: handle(current_directory / subdir), info['children'])
+
     template = latex_env.get_template(info['type'] + '.tex')
-    print(template.render(info = info))
+    return template.render(info = info)
 
 @main.command()
 def build():
     print(get_node_info(Path('.')))
-    handle(Path('.'))
+    print(handle(Path('.')))
 
