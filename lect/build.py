58be7c7b2382be8b9f7381d961e8e3306fb884d3
Markus Himmel <markus@himmel-villmar.de>
2020-06-12 12:14:06 +0200

    Split

---
+++ lect/build.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+from os import path
+import click, yaml, jinja2, tempfile, subprocess, shutil
+
+def get_node_info(current_directory):
+    with (current_directory / 'info.yaml').open() as f:
+        return yaml.safe_load(f)
+
+latex_env = jinja2.Environment(
+    loader = jinja2.PackageLoader(__name__, 'templates-latex'),
+    autoescape = False,
+    trim_blocks = True,
+    lstrip_blocks = True,
+    variable_start_string = '\VAR{',
+    variable_end_string = '}',
+    line_statement_prefix='%%',
+    block_start_string = '\BLOCK{',
+    block_end_string = '}')
+
+def read_file(fi):
+    with fi.open() as f:
+        s = f.read()
+    return s
+
+def handle(current_directory):
+    info = get_node_info(current_directory)
+    print(info)
+
+    if 'children' in info:
+        info['rendered_children'] = map(lambda subdir: handle(current_directory / subdir), info['children'])
+    try:
+        template = latex_env.get_template(info['type'] + '.tex')
+    except jinja2.exceptions.TemplateNotFound:
+        if 'children' in info:
+            template = latex_env.get_template('default-section.tex')
+        else:
+            template = latex_env.get_template('default.tex')
+
+    return template.render(info = info, readf = lambda s: read_file(current_directory / s), hasf = lambda s: (current_directory / s).exists())
+
+def build(output, tex):
+    code = handle(Path('.'))
+
+    if tex:
+        if output == 'lecture.pdf':
+            output = 'lecture.tex'
+        with open(output, 'w') as f:
+            f.write(code)
+        return
+
+    with tempfile.NamedTemporaryFile(mode='w', delete=False, suffix='.tex') as tmp:
+        tmp.write(code)
+        tmpn = tmp.name
+
+    with tempfile.TemporaryDirectory() as tmpd:
+        print(tmpd)
+        subprocess.run(['latexmk', '-pdf', tmpn, '-outdir=' + tmpd])
+        p = Path(tmpn)
+        shutil.copy(tmpd + '/' + p.stem + '.pdf', output)
+
+    Path(tmpn).unlink()
--- lect/cli.py
+++ lect/cli.py
@@ -1,69 +1,12 @@
-from pathlib import Path
-from os import path
-import click, yaml, jinja2, tempfile, subprocess, shutil
+import click, lect.build
 
 @click.group()
 def main():
     pass
 
-def get_node_info(current_directory):
-    with (current_directory / 'info.yaml').open() as f:
-        return yaml.safe_load(f)
-
-latex_env = jinja2.Environment(
-    loader = jinja2.PackageLoader(__name__, 'templates-latex'),
-    autoescape = False,
-    trim_blocks = True,
-    lstrip_blocks = True,
-    variable_start_string = '\VAR{',
-    variable_end_string = '}',
-    line_statement_prefix='%%',
-    block_start_string = '\BLOCK{',
-    block_end_string = '}')
-
-def read_file(fi):
-    with fi.open() as f:
-        s = f.read()
-    return s
-
-def handle(current_directory):
-    info = get_node_info(current_directory)
-    print(info)
-
-    if 'children' in info:
-        info['rendered_children'] = map(lambda subdir: handle(current_directory / subdir), info['children'])
-    try:
-        template = latex_env.get_template(info['type'] + '.tex')
-    except jinja2.exceptions.TemplateNotFound:
-        if 'children' in info:
-            template = latex_env.get_template('default-section.tex')
-        else:
-            template = latex_env.get_template('default.tex')
-
-    return template.render(info = info, readf = lambda s: read_file(current_directory / s), hasf = lambda s: (current_directory / s).exists())
-
 @main.command()
-@click.option('--output', default='lecture.pdf', help='Name of the output file')
-@click.option('--tex/--pdf', default=False, help='Produce LaTeX code instead of a PDF')
+@click.option('--output', default='lecture.pdf', help='Set the name of the output file.')
+@click.option('--tex/--pdf', default=False, help='Produce LaTeX code instead of a PDF.')
 def build(output, tex):
-    code = handle(Path('.'))
-
-    if tex:
-        if output == 'lecture.pdf':
-            output = 'lecture.tex'
-        with open(output, 'w') as f:
-            f.write(code)
-        return
-
-    with tempfile.NamedTemporaryFile(mode='w', delete=False, suffix='.tex') as tmp:
-        tmp.write(code)
-        tmpn = tmp.name
-
-    with tempfile.TemporaryDirectory() as tmpd:
-        print(tmpd)
-        subprocess.run(['latexmk', '-pdf', tmpn, '-outdir=' + tmpd])
-        p = Path(tmpn)
-        shutil.copy(tmpd + '/' + p.stem + '.pdf', output)
-
-    Path(tmpn).unlink()
+    lect.build.build(output, tex)
 
